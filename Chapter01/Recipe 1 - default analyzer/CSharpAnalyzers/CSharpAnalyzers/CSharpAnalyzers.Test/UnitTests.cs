353c2939a188690d716a080a372949213d3255c5
Prashant Mishra <prashantm@packtpub.com>
2017-07-28 17:00:59 +0530

    byb Packt

---
+++ Chapter01/Recipe 1 - default analyzer/CSharpAnalyzers/CSharpAnalyzers/CSharpAnalyzers.Test/UnitTests.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestHelper;
+using CSharpAnalyzers;
+
+namespace CSharpAnalyzers.Test
+{
+    [TestClass]
+    public class UnitTest : CodeFixVerifier
+    {
+        //No diagnostics expected to show up
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var test = @"class CLASS1 { }";
+
+            var expected = new DiagnosticResult
+            {
+                Id = "CSharpAnalyzers",
+                Message = String.Format("Type name '{0}' contains lowercase letters", "Class1"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 11, 15) }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {   
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = "CSharpAnalyzers",
+                Message = String.Format("Type name '{0}' contains lowercase letters", "TypeName"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 11, 15)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+
+            var fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TYPENAME
+        {   
+        }
+    }";
+            VerifyCSharpFix(test, fixtest);
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new CSharpAnalyzersCodeFixProvider();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new CSharpAnalyzersAnalyzer();
+        }
+    }
+}
